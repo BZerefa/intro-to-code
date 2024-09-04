aa087bdd0bb803bcccd8304b2915a31613037dba
BZerefa <zereb525@newschool.edu>
2024-09-04 14:48:43 -0400

    trying again

---
+++ unti-1/lesson-1/command_line_output.py
@@ -0,0 +1,5 @@
+# Changed i=2 so it starts at 2
+i = 0
+while i > -10:
+    print( i )
+    i = i - 1
---
+++ unti-1/lesson-1/test.py
@@ -0,0 +1,5 @@
+x = 5
+if x > 5:
+    print("Greater than fiiiivee!!!!!!")
+else:
+    print(":) less than 5")
