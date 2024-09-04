8431ad48c19b722484548c88e32405b9b9c36801
BZerefa <zereb525@newschool.edu>
2024-09-04 16:57:05 -0400

    added untracked files

---
+++ unti-1/lesson-2/classwork.py
@@ -0,0 +1,23 @@
+# this is a statement
+name = "beimnet"
+# press play after and terminal should say beimnet
+print(name)
+# make a counter
+count = 0
+
+print(count)
+# adding 1 to 0
+count = count + 1
+# press play to print 0 and 1
+print(count)
+# make a list
+names = ["Beimnet", "Badah", "Kirsten", "Cate"]
+# len is short for length
+numberofnames = len(names)
+# press play to print 0,1,4
+print(numberofnames)
+# 0 is the first item on a list
+print(names[0])
+# for loop 
+for people in names:
+    print(people)
